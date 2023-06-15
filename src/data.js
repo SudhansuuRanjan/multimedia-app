1d1945c60303bb5f8bd0c497b258fd7d561c2b64
SudhansuuRanjan <sudhanshur.ug20.ee@nitp.ac.in>
2023-06-15 18:59:02 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,38 @@
+export const data = [
+    {
+        id: 1,
+        name: 'Butterfly Video',
+        type: 'video',
+        path: "/file-server/video.mp4"
+    },
+    {
+        id: 2,
+        name: 'Music',
+        type: 'audio',
+        path: "/file-server/audio.mp3"
+    },
+    {
+        id: 3,
+        name: 'Java Notes',
+        type: 'document',
+        path: "/file-server/document.pdf"
+    },
+    {
+        id: 4,
+        name: 'Cat Image',
+        type: 'image',
+        path: "/file-server/cat.jpg"
+    },
+    {
+        id: 5,
+        name: 'Folders Image',
+        type: 'image',
+        path: "/file-server/folder.jpg"
+    },
+    {
+        id: 6,
+        name: 'Sunset Image',
+        type: 'image',
+        path: "/file-server/scene.png"
+    }
+]
